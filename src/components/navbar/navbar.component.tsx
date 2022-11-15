6e5505f02b4f0550a31b0e0014a9b2213a12b881
a.ghader <a.ghader@entelligence365.com>
2022-11-15 21:56:50 +0200

    feat(navbar component): navbar component

--- src/components/navbar/navbar.component.tsx
+++ src/components/navbar/navbar.component.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { ArrowRightIcon } from '@heroicons/react/24/solid';
 
 const Navbar = () => {
-  return <div />
+  const { t } = useTranslation(['navbar']);
+
+  return (
+    <header className="bg-gray-800 md:sticky top-0 z-10">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="title-font font-medium text-white mb-4 md:mb-0">
+          <a href="#about" className="ml-3 text-xl">
+            Alaa Ghader
+          </a>
+        </a>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+          <a href="#projects" className="mr-5 hover:text-white">
+            {t('PAST_WORK')}
+          </a>
+          <a href="#skills" className="mr-5 hover:text-white">
+            {t('SKILLS')}
+          </a>
+        </nav>
+        <a
+          href="#contact"
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+        >
+          {t('CONTACT_ME')}
+          <ArrowRightIcon className="w-4 h-4 ml-1" />
+        </a>
+      </div>
+    </header>
+  );
 };
 
-export { Navbar }
+export { Navbar };
