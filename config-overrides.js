f5b72fa8cf9eb3bfc1851f79e2f1411d336dbfd7
a.ghader <a.ghader@entelligence365.com>
2022-11-15 22:44:52 +0200

    feat(about component): about component

--- config-overrides.js
+++ config-overrides.js
@@ -7,6 +7,7 @@ module.exports = function override(config) {
       '&locales': path.resolve(__dirname, 'src/locales'),
       '&config': path.resolve(__dirname, 'src/config'),
       '&components': path.resolve(__dirname, 'src/components'),
+      '&assets': path.resolve(__dirname, 'src/assets'),
     },
   };
   return config;
--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,7 +1,9 @@
 import { navbarNameSpace } from "&components/navbar/navbar.i18n"
+import { aboutNameSpace } from "&components/about/about.i18n"
 
 const enNamespace = {
-    navbar: navbarNameSpace.en
+    navbar: navbarNameSpace.en,
+    about: aboutNameSpace.en
 };
 
 export default enNamespace;
