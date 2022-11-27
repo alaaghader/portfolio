0cccb3566ae2f0969694135ed1284510238711c6
a.ghader <a.ghader@entelligence365.com>
2022-11-27 18:20:43 +0200

    feat: skills

---
+++ src/components/skills/skills.data.ts
@@ -0,0 +1,3 @@
+const skills: Array<string> = ["React JS", "React native", "Node JS", "Flutter", ".NET", "SQL", "MongoDB", "GIT"];
+
+export {skills}
--- src/locales/ar.ts
+++ src/locales/ar.ts
@@ -1,9 +1,11 @@
 import { navbarNameSpace } from "&components/navbar/navbar.i18n"
 import { aboutNameSpace } from "&components/about/about.i18n"
+import { skillsNameSpace } from "&components/skills/skills.i18n";
 
 const arNamespace = {
     navbar: navbarNameSpace.ar,
-    about: aboutNameSpace.ar
+    about: aboutNameSpace.ar,
+    skills: skillsNameSpace.ar
 };
 
 export default arNamespace;
